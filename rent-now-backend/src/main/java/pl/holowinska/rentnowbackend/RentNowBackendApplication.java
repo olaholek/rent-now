1f29af2109fd24c42c2e14ca084407481c8908be
olaholek <olka112000@gmail.com>
2023-07-24 21:14:54 +0200

    initial commit backend

---
+++ rent-now-backend/src/main/java/pl/holowinska/rentnowbackend/RentNowBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.holowinska.rentnowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentNowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentNowBackendApplication.class, args);
+    }
+
+}
---
+++ rent-now-backend/src/test/java/pl/holowinska/rentnowbackend/RentNowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.holowinska.rentnowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentNowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
